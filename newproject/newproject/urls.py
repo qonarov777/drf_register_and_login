a3cf5b13e56732a6a0e23b1f42d48ad275ee464e
shohrux0113 <qonarov777@gmail.com>
2022-10-14 22:53:44 +0500

    new

--- newproject/newproject/urls.py
+++ newproject/newproject/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('registerapp.urls')),
+    path('', include('newapp.urls')),
     
 ]
 
